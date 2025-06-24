d258a18f5fe8015b6421a5f501a72aa6b1b2dee5
mohrip <m25srt@gmail.com>
2025-06-24 10:33:49 +0300

    ""

---
+++ backend/user/src/main/java/com/user/user/config/MongoConfig.java
@@ -0,0 +1,18 @@
+package com.user.user.config;
+
+    
+import com.mongodb.client.MongoClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+@Configuration
+public class MongoConfig {
+
+    @Bean
+    @Primary 
+    public MongoTemplate mongoTemplate(MongoClient mongoClient) {
+        return new MongoTemplate(mongoClient, "mydatabase");
+    }
+}
