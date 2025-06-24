2ccf11c8fb6209d606e82bb20a4fd7ae6e6a01b3
mohrip <m25srt@gmail.com>
2025-06-24 08:08:58 +0300

    ""

---
+++ backend/user/src/main/java/com/user/user/exception/CustomerNotFoundException.java
@@ -0,0 +1,11 @@
+package main.java.com.user.user.exception;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class CustomerNotFoundException extends RuntimeException {
+
+    private final String msg;
+}
