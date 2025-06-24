879616140b9e586afa014b9119347be910d2c826
mohrip <m25srt@gmail.com>
2025-06-24 08:17:59 +0300

    ""

--- backend/user/src/main/java/com/user/user/customer/CustomerResponse.java
+++ backend/user/src/main/java/com/user/user/customer/CustomerResponse.java
@@ -1,3 +1,12 @@
 package main.java.com.user.user.customer;
     
 
+
+public record CustomerResponse(
+        String id,
+        String firstname,
+        String lastname,
+        String email,
+        Address address
+) {
+}
