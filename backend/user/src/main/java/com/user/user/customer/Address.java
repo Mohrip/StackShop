e10088316804feeaaa0d6a40333cf6f6e0aa3c67
mohrip <m25srt@gmail.com>
2025-06-24 08:11:07 +0300

    ""

--- backend/user/src/main/java/com/user/user/customer/Address.java
+++ backend/user/src/main/java/com/user/user/customer/Address.java
@@ -1,3 +1,19 @@
+package main.java.com.user.user.customer;
+
+import lombok.*;
+import org.springframework.validation.annotation.Validated;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Validated
 public class Address {
-    
+    private String street;
+    private String city;
+    private String state;
+    private String houseNumber;
+    private String zipCode;
+    private String country;
 }
