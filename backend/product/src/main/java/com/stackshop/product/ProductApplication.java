1e324112e7eae5141b39305e801ab361f33a1ab6
Mohammed Alsaif <123654677+Mohrip@users.noreply.github.com>
2025-07-14 07:49:42 +0300

    Update ProductApplication.java

--- backend/product/src/main/java/com/stackshop/product/ProductApplication.java
+++ backend/product/src/main/java/com/stackshop/product/ProductApplication.java
@@ -2,7 +2,7 @@ package com.stackshop.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-    
+     
 @SpringBootApplication(scanBasePackages = {"com.stackshop.product", "com.stackshop.fakeStore"})
 public class ProductApplication {
 
